c430069de89373031586f2ddbd7469121401137d
591friend@gmail.com <591friend@gmail.com@a02a22e2-e5b8-80db-486a-d1ad1419a516>
2012-12-26 10:43:36 +0000

    对Razor进行相应的修改

---
+++ trunk/HiGirl360/Areas/Manager/Controllers/MenuManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiGirl360.Areas.Manager.Controllers
+{
+    public class MenuManagerController : Controller
+    {
+        //
+        // GET: /Manager/MenuManager/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
